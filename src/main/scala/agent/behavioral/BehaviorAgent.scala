35392ba57d825729278a70a4da7b9de8cc00777a
Condere Jerome <jerome.condere@gmail.com>
2017-05-21 23:11:28 +0200

    add props functions for behaviors

--- src/main/scala/agent/behavioral/BehaviorAgent.scala
+++ src/main/scala/agent/behavioral/BehaviorAgent.scala
@@ -1,6 +1,28 @@
 package agent.behavioral
-import agent.NostalgiaAgent
+import scala.collection.mutable.ArrayBuffer
+import behavior.proxy._
 
-class BehaviorAgent extends NostalgiaAgent {
+import agent.NostalgiaAgent
+import behavior.AbstractBehavior
+import akka.actor.FSM
+import akka.actor.ActorRef
+
+sealed trait BehaviorAgentState
+case object Idle
+case object Ended
+case object Error
+case object BehaviorActive
+
+sealed trait BehaviorAgentData
+sealed trait BehaviorAgentMessage
+
+
+abstract class BehaviorAgent extends NostalgiaAgent with FSM[BehaviorAgentState,BehaviorAgentData] {
+  val behaviors: ArrayBuffer[ActorRef] = ArrayBuffer.empty
+  val behavior: ArrayBuffer[BehaviorProxy[AbstractBehavior]]
+  
+  def addBehavior(behaviorProxy: AbstractBehavior) = {
+    //behaviors
+  }
   
 }
