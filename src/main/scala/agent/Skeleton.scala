2f281a92e09f9dc8db38bdc53c960772655a1887
Condere Jerome <jerome.condere@gmail.com>
2017-05-22 00:05:37 +0200

    add state for behavior agent\n maybe use trait

--- src/main/scala/agent/Skeleton.scala
+++ src/main/scala/agent/Skeleton.scala
@@ -1,6 +1,6 @@
 package agent
 import scala.reflect.ClassTag 
-
+/**A class use to reconstruct a Nostalgia agent with the parameters*/
 abstract class Skeleton[T <: NostalgiaAgent : ClassTag]{
  def agent:T
 }
--- src/main/scala/agent/behavioral/BehaviorAgent.scala
+++ src/main/scala/agent/behavioral/BehaviorAgent.scala
@@ -6,23 +6,72 @@ import agent.NostalgiaAgent
 import behavior.AbstractBehavior
 import akka.actor.FSM
 import akka.actor.ActorRef
+import akka.actor.Props
+import scala.reflect.ClassTag 
 
 sealed trait BehaviorAgentState
-case object Idle
-case object Ended
-case object Error
-case object BehaviorActive
+case object Idle extends BehaviorAgentState
+case object Ready extends BehaviorAgentState
+case object Running extends BehaviorAgentState
+case object Ended extends BehaviorAgentState
+case object Error extends BehaviorAgentState
+case object BehaviorActive extends BehaviorAgentState
 
-sealed trait BehaviorAgentData
 sealed trait BehaviorAgentMessage
+case object Setup extends BehaviorAgentMessage
+case object Run extends BehaviorAgentState
+case object Next extends BehaviorAgentState
+case object End extends BehaviorAgentState
 
 
-abstract class BehaviorAgent extends NostalgiaAgent with FSM[BehaviorAgentState,BehaviorAgentData] {
+abstract class BehaviorAgent extends NostalgiaAgent with FSM[BehaviorAgentState,Int] {
   val behaviors: ArrayBuffer[ActorRef] = ArrayBuffer.empty
-  val behavior: ArrayBuffer[BehaviorProxy[AbstractBehavior]]
+  val behaviorsProxyList: ArrayBuffer[BehaviorProxy[AbstractBehavior]]
   
-  def addBehavior(behaviorProxy: AbstractBehavior) = {
-    //behaviors
+  final def addBehavior[A <: AbstractBehavior : ClassTag](behaviorProxy: BehaviorProxy[A]) = {
+    //behaviorsProxyList+= behaviorProxy
   }
+  
+  def setup = {
+    behaviorsProxyList.zipWithIndex.foreach{
+      case(behaviorProxy,index) => val behavior = context.actorOf(Props(behaviorProxy.behavior()),"") 
+                                   behaviors += behavior
+    }
+  }
+  
+  when(Idle)
+  {
+     case  Event(Setup,_) => setup
+                             goto(Ready)
+                                  
+  }
+  
+  when(Ready)
+  {
+    case  Event(Run,_) => self ! Run
+                          goto(Running) using 0
+  }
+  
+  when(Running)
+  {
+    case Event(behavior.Finished, index) => if(index < behaviors.size -2)
+                                            {
+                                              self ! Run
+                                              stay using index + 1
+                                            } else {
+                                              self ! End
+                                              goto(Ended)
+                                            }
+                                            
+    case  Event(Run, index) => behaviors(index) ! behavior.Setup()
+                               behaviors(index) ! behavior.Run
+                               stay
+  }
+  
+  when (Ended)
+  {
+     case  Event(End, _) => stop()
+  }
+  
   
 }
