bbc4c3e36eb369e70d7bdc7d665a96cb1adc24b9
Condere Jerome <jerome.condere@gmail.com>
2017-05-23 21:16:24 +0200

    completing behaviorProxy test

--- src/test/scala/BehaviorProxySpec.scala
+++ src/test/scala/BehaviorProxySpec.scala
@@ -3,11 +3,16 @@ import behavior._
 import behavior.proxy.BehaviorProxy
 import akka.actor._
 import akka.testkit._
+import scala.concurrent.duration._
 
 class BehaviorProxySpec extends TestKit(ActorSystem("MySpec")) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll {
   
   implicit val systemSupervisor = self
+ 
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
   
   "A behavior proxy" must {
     "store a behavior" in {
@@ -23,9 +28,11 @@ class BehaviorProxySpec extends TestKit(ActorSystem("MySpec")) with ImplicitSend
         }
         
         val beRef = TestActorRef (behaviorProxy.behavior() )
-        //TODO assert a=5
-        
-        //TODO run + assert a=8       
+        beRef ! Setup()
+        awaitCond(a == 4, 70 millis)
+        beRef ! Run
+        awaitCond(a == 7, 70 millis)
+        expectMsg(Finished)
       }
     }
   
