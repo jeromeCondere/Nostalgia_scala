54b6cc2bd269dc88e8f71f8f9111b8758545e068
Condere Jerome <jerome.condere@gmail.com>
2017-05-27 22:25:09 +0200

    add list sort example + correct list feature

---
+++ src/example/scala/collections/ListSort.scala
@@ -0,0 +1,38 @@
+package collections
+import agent.list._
+import akka.actor._
+import akka.pattern.{ ask, pipe }
+import scala.concurrent._;
+import scala.util.{Success, Failure}
+import akka.util.Timeout
+import scala.concurrent.duration._
+
+class SortedListAgent(val l: List[Int]) extends ListAgent(l.sortWith((a,b) => a < b)) {
+  
+  override def collide(a: ActorRef) = {
+    import context.dispatcher
+    val f: Future[List[Int]] =  for {
+      z1 <- (a ? GetAll).mapTo[List[Int]]
+      z = z1:::l
+    } yield z
+    
+     
+   f.map { newlist => context.actorOf(Props(new SortedListAgent(newlist)))}
+  }
+}
+
+object ListSort extends App {
+  implicit val timeout = Timeout(2 seconds)
+  import ExecutionContext.Implicits.global
+  
+  val system = ActorSystem("mySystem")
+  val myActorSortedList = system.actorOf(Props(new SortedListAgent(List(1,9,6))), "myActorSortedList")
+  val myActorSortedList2 = system.actorOf(Props(new SortedListAgent(List(11,-9,5))), "myActorSortedList2")
+  
+  val f: Future[ActorRef] = (myActorSortedList ? Collide(myActorSortedList2)).mapTo[ActorRef]
+   f onComplete {
+    case Success(v) => v ! Foreach(println)
+    case Failure(e) => print(e.getMessage)
+  }
+  
+}
--- src/main/scala/agent/list/ListAgent.scala
+++ src/main/scala/agent/list/ListAgent.scala
@@ -5,6 +5,8 @@ import akka.actor.ActorRef
 import scala.concurrent.duration._
 import akka.util.Timeout
 import scala.concurrent.Future;
+import scala.util.{Success, Failure}
+import akka.pattern.pipe
 
 trait ListAction
 case class Get(index: Int) extends ListAction
@@ -14,6 +16,7 @@ case class Concat[B](l:List[B]) extends ListAction
 case class Filter[A](p: (A) => Boolean) extends ListAction
 case class Collide(a: ActorRef) extends ListAction
 case class Map[A,B](f: (A) => B ) extends ListAction
+case class Foreach[A](f: (A) => Unit) extends ListAction
 
 class ListAgent[A](l: List[A]) extends NostalgiaAgent {
   implicit val timeout = Timeout(2 seconds)
@@ -22,22 +25,21 @@ class ListAgent[A](l: List[A]) extends NostalgiaAgent {
  override def move = self
  override def collide(actor :ActorRef) = Future{self}
  override def emit = Some(context.actorOf(Props(new ListAgent(l))))
-  
-  def receive = {
+
+ def receive = {
     case Get(i) => sender ! l(i)
     case GetAll => sender ! l
     case GetClass => sender ! this.getClass
-    case Concat(li) => val a = Props(new ListAgent(l::li))
-                       sender ! context.actorOf(a)
+    case Concat(li) => sender ! context.actorOf(ListAgent.props(l::li))
                        
-    case Collide(a) => sender ! collide(a)
+    case Collide(a) => collide(a).pipeTo(sender)
     
-    case Filter(p: ((A) => Boolean)) => val a = Props(new ListAgent(l.filter(p)))
-                                        sender ! context.actorOf(a)
-    case Map(f: (A => Any)) => val a = Props(new ListAgent(l.map(f)))
-                                        sender ! context.actorOf(a)
-    
-
+    case Filter(p: ((A) => Boolean)) => sender ! context.actorOf(ListAgent.props(l.filter(p)))
+    case Map(f: (A => Any)) => sender ! context.actorOf(ListAgent.props(l.map(f)))
+    case Foreach(f: ((A) => Unit)) =>  l.foreach(f)
                        
   }
+}
+object ListAgent {
+  def props[A](l: List[A])= Props(new ListAgent(l))
 }
