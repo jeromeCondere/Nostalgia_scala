db1b6b6fed38f47e549369b496e5035518b6091b
alpha-omega <jerome.condere@gmail.com>
2018-03-18 19:24:10 +0100

    netlogo plot agent - must add implicits conversions

--- src/main/scala/agent/simulation/graphical/netlogo/NetlogoConstants.scala
+++ src/main/scala/agent/simulation/graphical/netlogo/NetlogoConstants.scala
@@ -15,9 +15,19 @@ object NetlogoConstants {
   val DEFAULT_SLIDER_MIN_VALUE = 0f
   val DEFAULT_SLIDER_MAX_VALUE = 100f
   
-  //
+  //switch constants
   val DEFAULT_SWITCH_SIZE = 130
   val DEFAULT_SWITCH_VALUE = false
+  
+  //plot constants
+  val DEFAULT_PLOT_WIDTH = 200
+  val DEFAULT_PLOT_HEIGHT = 200
+  val DEFAULT_PLOT_XLABEL = "x"
+  val DEFAULT_PLOT_YLABEL = "y"
+  val DEFAULT_PLOT_XMIN = 0
+  val DEFAULT_PLOT_XMAX = 100
+  val DEFAULT_PLOT_YMIN = 0
+  val DEFAULT_PLOT_YMAX = 100
   
   
 }
---
+++ src/main/scala/agent/simulation/graphical/netlogo/component/NetlogoPlotAgent.scala
@@ -0,0 +1,144 @@
+package agent.simulation.graphical.netlogo.component
+import agent.simulation.graphical.GraphicalParam
+import agent.simulation.graphical.netlogo.NetlogoModel
+import agent.simulation.graphical.netlogo.NetlogoSimpleListener
+import agent._
+import java.awt.Point
+import org.nlogo.api.Version
+import agent.simulation.graphical.netlogo.{NetlogoConstants => NC}
+
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Set
+
+case class PlotLimits(xmin: Double, xmax: Double, ymin: Double, ymax: Double)
+case class PlotLabels(xlabel: String, ylabel: String)
+case class PlotMetaData(
+    limits: PlotLimits = PlotLimits(
+          NC.DEFAULT_PLOT_XMIN,
+          NC.DEFAULT_PLOT_XMAX,
+          NC.DEFAULT_PLOT_YMIN,
+          NC.DEFAULT_PLOT_YMAX
+        ),
+    labels: PlotLabels = PlotLabels("x","y")
+)
+
+class PlotModel(params: GraphicalParam, val name: String, val width: Int, val height: Int, val metadata: PlotMetaData,x: Any*) extends NetlogoModel(params,"", x) {
+  
+}
+
+object PlotModel {
+  def apply(params: GraphicalParam, name: String, width: Int = NC.DEFAULT_PLOT_WIDTH, height: Int = NC.DEFAULT_PLOT_HEIGHT, metadata: PlotMetaData = PlotMetaData()) = new PlotModel(params, name, width, height, metadata)
+}
+
+case class PlotPen(updateCommand: String) 
+
+/**
+ * A netlogo plot agent
+ */
+class NetlogoPlotAgent (plotModel: PlotModel)(maxTicks:Int = NC.DEFAULT_MAX_TICKS)(fps: Float = NC.DEFAULT_FPS) extends NetlogoAgentComponent(plotModel)(maxTicks)(fps) with Simple {
+  private val pens: ListBuffer[PlotPen] = ListBuffer.empty[PlotPen]
+  private val variables: Set[String] = Set.empty[String]
+  
+  def addPlotPens(pens: PlotPen*) = this.pens ++= pens
+  def addPlotVariables(variables: String*) = this.variables ++= variables
+  
+  def setup = {}
+  def check = {}
+  def variablesPlot = {
+    var varStr = ""
+    variables.foreach{v => varStr += v+" "}
+    varStr
+  }
+  def pensPlot = {
+    var pensStr = ""
+    pens.zipWithIndex.foreach{
+      case(p,i) => pensStr += s""" "${plotModel.name+"_pen"+i}" 1.0 0 -16777216 true "" "${p.updateCommand}"\n"""
+    }
+    pensStr
+  }
+  private def stringOrNil(s: String) = if(s.isEmpty) s else "NIL"
+  override final def runNetlogo = {
+     val eps = 20
+     wait {
+        frame.setSize(plotModel.width + eps, plotModel.height + eps)
+        frame.setLocation(new Point(plotModel.params.pos._1, plotModel.params.pos._2))
+        frame.add(comp)
+        frame.setVisible(true)
+        frame.setResizable(false)
+        comp.openFromSource("plot", "", modelPlot)
+      }
+      cmd("setup")
+    }
+    def modelPlot = {s"""
+globals [${variablesPlot}]
+
+to setup
+ ca
+ reset-ticks
+end
+to go
+ tick
+end
+@#$$#@#$$#@
+GRAPHICS-WINDOW
+10
+166
+255
+214
+16
+1
+5.7
+1
+10
+1
+1
+1
+0
+1
+1
+1
+-16
+16
+-1
+1
+0
+0
+1
+ticks
+$fps
+
+PLOT
+0
+0
+${plotModel.width}
+${plotModel.height}
+${plotModel.name}
+${stringOrNil(plotModel.metadata.labels.xlabel)}
+${stringOrNil(plotModel.metadata.labels.ylabel)}
+${plotModel.metadata.limits.xmin}
+${plotModel.metadata.limits.xmax}
+${plotModel.metadata.limits.ymin}
+${plotModel.metadata.limits.ymax}
+true
+false
+"" ""
+PENS
+${pensPlot}
+
+
+@#$$#@#$$#@
+@#$$#@#$$#@
+@#$$#@#$$#@
+${Version.version}
+@#$$#@#$$#@
+@#$$#@#$$#@
+@#$$#@#$$#@
+@#$$#@#$$#@
+@#$$#@#$$#@
+@#$$#@#$$#@
+0
+@#$$#@#$$#@
+"""
+  }
+  
+}
